fd7e83974c96cc28826a0dbc25eeee3c01d6aee1
Site Builder <builder@example.com>
2026-06-01 11:56:00 -0400

    our mission section

--- wp-content/themes/alisonsacupuncture/front-page.php
+++ wp-content/themes/alisonsacupuncture/front-page.php
@@ -10,7 +10,6 @@ get_header();
 <main id="main" class="site-main">
   <?php get_template_part('template-parts/front-page/hero'); ?>
   <?php get_template_part('template-parts/front-page/hours'); ?>
-  <?php get_template_part('template-parts/front-page/intro'); ?>
   <?php get_template_part('template-parts/front-page/about'); ?>
   <?php get_template_part('template-parts/front-page/services'); ?>
   <?php get_template_part('template-parts/front-page/contact'); ?>
