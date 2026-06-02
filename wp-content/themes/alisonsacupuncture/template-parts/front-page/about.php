c5a462490c666c999e7acd9bc8592c37ad4c64f3
Cameron Brown <cam.brown94@gmail.com>
2026-06-02 13:51:53 -0400

    services section

--- wp-content/themes/alisonsacupuncture/template-parts/front-page/about.php
+++ wp-content/themes/alisonsacupuncture/template-parts/front-page/about.php
@@ -9,15 +9,13 @@ $body = get_field('about_text');
 $bg_image_id = get_field('about_image');
 $bg_image_url = $bg_image_id ? wp_get_attachment_image_url($bg_image_id, 'full') : '';
 
-do_action('qm/debug', $bg_image_url);
-
 ?>
 <section id="about" class="about-section">
   <img class="about-bg" src="<?php echo esc_url($bg_image_url); ?>" alt="<?php echo esc_attr($title); ?>">
   <div class="cntr left">
     <div class="about-content">
       <div class="about-text">
-        <h2 class="text-shadow"><?php echo esc_html($title); ?></h2>
+        <h2 class="text-3d-shadow"><?php echo esc_html($title); ?></h2>
         <p><?php echo esc_html($body); ?></p>
       </div>
     </div>
--- wp-content/themes/alisonsacupuncture/template-parts/front-page/services.php
+++ wp-content/themes/alisonsacupuncture/template-parts/front-page/services.php
@@ -3,30 +3,52 @@
 /**
  * Front Page Services Section
  */
+
+$services = [
+  'acupuncture'          => ['title' => 'acupuncture_title',          'desc' => 'acupuncture_description',          'img' => 'acupuncture_image'],
+  'electroacupuncture'   => ['title' => 'electroacupuncture_title',   'desc' => 'electroacupuncture_description',   'img' => 'electroacupuncture_image'],
+  'dry_needling'         => ['title' => 'dryneedling_title',          'desc' => 'dryneedling_description',          'img' => 'dryneedling_image'],
+  'cupping'              => ['title' => 'cupping_title',              'desc' => 'cupping_description',              'img' => 'cupping_image'],
+  'tissue_work'          => ['title' => 'tissuework_title',           'desc' => 'tissuework_description',           'img' => 'tissuework_image'],
+  'lower_tissue_work'    => ['title' => 'lowertissuework_title',      'desc' => 'lowertissuework_description',      'img' => 'lowertissuework_image'],
+  'guasha'               => ['title' => 'guasha_title',               'desc' => 'guasha_description',               'img' => 'guasha_image'],
+  'earwax'               => ['title' => 'earwax_title',               'desc' => 'earwax_description',               'img' => 'earwax_image'],
+  'balancecare'          => ['title' => 'balancecare_title',          'desc' => 'balancecare_description',          'img' => 'balancecare_image'],
+  'medication_education' => ['title' => 'medication_education_title', 'desc' => 'medication_education_description', 'img' => 'medication_education_image'],
+  'heart_health'         => ['title' => 'heart_health_title',         'desc' => 'heart_health_description',         'img' => 'heart_health_image'],
+];
+
+$logo = wp_get_attachment_image(141, 'full', false, ['class' => 'service-card-logo']);
+do_action('qm/debug', $logo);
+
 ?>
 <section id="services" class="services-section">
   <div class="cntr">
-    <h2><?php the_field('services_title'); ?></h2>
+    <h2 class="text-3d-shadow"><?php the_field('services_title'); ?></h2>
     <div class="services-grid">
-      <?php for ($i = 1; $i <= 4; $i++) :
-        $service_name = get_field("service_{$i}_name");
-        $service_description = get_field("service_{$i}_description");
-        $service_icon = get_field("service_{$i}_icon");
-        if ($service_name || $service_description) :
+      <?php foreach ($services as $key => $fields) :
+        $group = get_field($key);
+        $title = $group[$fields['title']] ?? '';
+        $description = $group[$fields['desc']] ?? '';
+        $image_id = $group[$fields['img']] ?? '';
+        $image_url = $image_id ? wp_get_attachment_image_url($image_id, 'large') : '';
       ?>
-          <div class="service-card">
-            <?php if ($service_icon) : ?>
-              <div class="service-icon"><?php echo esc_html($service_icon); ?></div>
-            <?php endif; ?>
-            <?php if ($service_name) : ?>
-              <h3><?php echo esc_html($service_name); ?></h3>
-            <?php endif; ?>
-            <?php if ($service_description) : ?>
-              <p><?php echo esc_html($service_description); ?></p>
-            <?php endif; ?>
+        <div class="service-card" tabindex="0">
+          <div class="service-card-inner">
+            <div class="service-card-front" <?php echo $image_url ? ' style="background-image: url(' . esc_url($image_url) . ')"' : ''; ?>>
+              <h3 class="text-3d-shadow"><?php echo esc_html($title); ?></h3>
+            </div>
+            <div class="service-card-back">
+              <?php if ($logo) : ?>
+                <?php echo $logo; ?>
+              <?php endif; ?>
+              <?php if ($description) : ?>
+                <p><?php echo esc_html($description); ?></p>
+              <?php endif; ?>
+            </div>
           </div>
-      <?php endif;
-      endfor; ?>
+        </div>
+      <?php endforeach; ?>
     </div>
   </div>
 </section>
