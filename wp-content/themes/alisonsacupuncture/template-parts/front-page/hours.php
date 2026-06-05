de9e21d396e9f0d26459f2cb037b5d65b0410bcd
Cameron Brown <cam.brown94@gmail.com>
2026-06-04 22:00:09 -0400

    better hours section background

--- wp-content/themes/alisonsacupuncture/template-parts/front-page/hours.php
+++ wp-content/themes/alisonsacupuncture/template-parts/front-page/hours.php
@@ -16,7 +16,6 @@ $hours_alt_txt = get_post_meta($img_id, '_wp_attachment_image_alt', true);
 
 <section class="hours-section" id="hours">
 
-
   <?php if (!empty($hours_image)) : ?>
     <div class="hours-image">
       <img
@@ -26,7 +25,9 @@ $hours_alt_txt = get_post_meta($img_id, '_wp_attachment_image_alt', true);
     </div>
   <?php endif; ?>
   <div class="hours-layout">
-    <div class="hours-blob hours-blob--top-left"></div>
+    <svg class="hours-blob-svg hours-blob-svg--top-left" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path d="M21 3v2c0 9.627-5.373 14-12 14H7.098c.212-3.012 1.15-4.835 3.598-7.001 1.204-1.065 1.102-1.68.509-1.327-4.084 2.43-6.112 5.714-6.202 10.958L5 22H3c0-1.363.116-2.6.346-3.732C3.116 16.974 3 15.218 3 13 3 7.477 7.477 3 13 3c2 0 4 1 8 0z"/>
+    </svg>
     <div class="hours-content">
       <h2 class="text-3d-shadow slide-in-right"><?php the_field('hours_title'); ?></h2>
       <table class="hours-table slide-in-right">
@@ -58,4 +59,8 @@ $hours_alt_txt = get_post_meta($img_id, '_wp_attachment_image_alt', true);
       <?php endif; ?>
     </div>
   </div>
+
+  <svg class="hours-blob-svg hours-blob-svg--bottom-right" viewBox="0 -32 576 576" xmlns="http://www.w3.org/2000/svg">
+    <path d="M546.2 9.7c-5.6-12.5-21.6-13-28.3-1.2C486.9 62.4 431.4 96 368 96h-80C182 96 96 182 96 288c0 7 .8 13.7 1.5 20.5C161.3 262.8 253.4 224 384 224c8.8 0 16 7.2 16 16s-7.2 16-16 16C132.6 256 26 410.1 2.4 468c-6.6 16.3 1.2 34.9 17.5 41.6 16.4 6.8 35-1.1 41.8-17.3 1.5-3.6 20.9-47.9 71.9-90.6 32.4 43.9 94 85.8 174.9 77.2C465.5 467.5 576 326.7 576 154.3c0-50.2-10.8-102.2-29.8-144.6z"/>
+  </svg>
 </section>
